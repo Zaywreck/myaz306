2734ee5975d3c73938ebdb1c0b0571d83cf578fa
Mert Gülle <91943281+Zaywreck@users.noreply.github.com>
2024-03-07 14:55:40 +0300

    week3

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import TimeWidget from './src/components/TimeWidget';
-import Counter from './src/components/Counter';
 import React from 'react'
 
 const App = () => {
     return (
         <View style={styles.container}>
-            <View style={styles.header}>
-                <TimeWidget />
-                <Counter />
-            </View>
-            <View style={styles.body}>
-                <Text>Merhaba</Text>
-            </View>
-            <View style={styles.footer}>
-                <Text>footer</Text>
-            </View>
+            <TimeWidget />
         </View>
     )
 }
@@ -25,24 +15,6 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#1F2544',
     },
-    header : {
-        flex: 3,
-        flexDirection: 'row',
-        backgroundColor: 'black',
-        justifyContent: 'center',
-    },
-    body: {
-        flex: 8,
-        backgroundColor: '#1F2544',
-        paddingTop: 40,
-    },
-    footer: {
-        flex: 1,
-        backgroundColor: '#474F7A',
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingBottom: 30
-    }
 })
 
 export default App
